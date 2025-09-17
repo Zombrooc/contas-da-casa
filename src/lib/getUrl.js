8f86fbe86337a2c183c5b3012c2b94d799c7ee75
Elian Valdez <elian.valdez09@gmail.com>
2025-09-16 21:07:58 -0300

    Applying http|https protocol to the application url

--- src/lib/getUrl.js
+++ src/lib/getUrl.js
@@ -1,5 +1,9 @@
 export function getUrl(path) {
-  const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || "";
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const protocol = isDevelopment ? "http://" : "https://";
+  const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `${protocol}${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "";
   const normalizedPath =
     path && !path.startsWith("/") ? `/${path}` : path || "";
   return `${baseUrl}${normalizedPath}`;
