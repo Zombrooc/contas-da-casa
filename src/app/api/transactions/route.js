b09761f66eb492babf5926a9e0fe5e0df8039baa
Elian Valdez <elian.valdez09@gmail.com>
2025-09-18 13:09:46 -0300

    Add some filter to the GET /transaction route

--- src/app/api/transactions/route.js
+++ src/app/api/transactions/route.js
@@ -1,10 +1,86 @@
 import { createTransaction } from "@/lib/actions/transactions/createTransaction";
 import { prisma } from "@/lib/prisma";
 import { createTransactionSchema } from "@/schemas/transactions";
+import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
+
+  const searchParams = req.nextUrl.searchParams
+
+  const filter = Object.fromEntries(searchParams)
+
+  const {
+    from,
+    to,
+    referenceMonth,
+    referenceYear,
+    categoryIds,
+    type,
+    minAmount,
+    maxAmount,
+  } = filter
+
+  const hasRange = from || to
+  const hasReference = referenceMonth && referenceYear
+
+  if (hasRange && hasReference) {
+    return NextResponse.json({
+      success: false,
+      error: ["Use apenas intervalo (from/to) OU mês/ano de referência, nunca ambos."]
+    })
+  }
+
+  const where = {}
+
+  if (hasRange) {
+    where.date = {
+      ...(from ? { gte: new Date(from) } : {}),
+      ...(to ? { lte: new Date(to) } : {}),
+    }
+  }
+
+  if (hasReference) {
+    const start = new Date(referenceYear, referenceMonth - 1, 1)
+    const end = new Date(referenceYear, referenceMonth, 0, 23, 59, 59)
+
+    where.createdAt = {
+      gte: start,
+      lte: end,
+    }
+  }
+
+  if (!hasReference && !hasRange) {
+    const now = new Date();
+    const referenceYear = now.getFullYear();
+    const referenceMonth = now.getMonth() + 1;
+    const start = new Date(referenceYear, referenceMonth - 1, 1);
+    const end = new Date(referenceYear, referenceMonth, 0, 23, 59, 59);
+
+    console.log('Chegou aqui sem range e sem referencia', referenceMonth, referenceYear)
+    where.createdAt = {
+      gte: start,
+      lte: end,
+    }
+  }
+
+  if (categoryIds?.length) {
+    where.categoryId = { in: categoryIds }
+  }
+
+  if (type) {
+    where.type = type
+  }
+
+  if (minAmount || maxAmount) {
+    where.amount = {
+      ...(minAmount ? { gte: minAmount } : {}),
+      ...(maxAmount ? { lte: maxAmount } : {}),
+    }
+  }
+
   const transactions = await prisma.transactions.findMany({
+    where,
     include: {
       wallet: true,
     },
