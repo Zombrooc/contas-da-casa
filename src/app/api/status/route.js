45bc641dab374e157799a1756a1deabdd0d2452e
Elian Valdez <elian.valdez09@gmail.com>
2025-09-23 15:19:06 -0300

    fix: fixing env var name

--- src/app/api/status/route.js
+++ src/app/api/status/route.js
@@ -12,7 +12,7 @@ export async function GET(req) {
   const databaseMaxConnectionsResult = await prisma.$queryRaw`SHOW max_connections;`;
   const databaseMaxConnectionsValue = databaseMaxConnectionsResult[0].max_connections
 
-  const databaseName = process.env.POSTGRES_DB;
+  const databaseName = process.env.PGDATABASE;
   const databaseOpenedConnectionResult = await prisma.$queryRaw`SELECT count(*)::int FROM pg_stat_activity WHERE datname= ${databaseName};`
   const databaseOpenedConnectionValue = databaseOpenedConnectionResult[0].count
 
